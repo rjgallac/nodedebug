06592603a86fa5c712063cd0794237be93dc1850
rjgallac <rjgallac@hotmail.com>
2026-04-07 17:50:24 +0100

    database work

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,26 @@ import express from 'express'
 const app = express()
 const port = 3000
 import type User from './User.ts';
+import pgPromise from 'pg-promise';
+
+const pgp = pgPromise({}); // Initialization options
+const db = pgp('postgres://postgres:password@nodedebug-postgres:5432/mydb');   
 
 app.get('/', (req, res) => {
   let user: User = {
     name: 'John Doe',
     id: 1
   };
+
+  db.one('SELECT $1 AS value', 123)
+    .then((data) => {
+      console.log('DATA:', data.value)
+    })
+    .catch((error) => {
+      console.log('ERROR:', error)
+    })
+
+
   res.send('Hello World!')
 })
 
