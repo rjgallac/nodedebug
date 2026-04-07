aeea2e4adf7240ccd29b52714351f46d91d077cd
rjgallac <rjgallac@hotmail.com>
2026-04-07 19:29:02 +0100

    products json from db

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM node:24
 WORKDIR /app
 COPY ./package.json /app
+COPY ./tsconfig.json /app
+COPY ./src /app/src
 RUN cd /app && npm install
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,36 @@
 import express from 'express'
 const app = express()
 const port = 3000
-import type User from './User.ts';
+import type { Product } from './Product.js';
 import pgPromise from 'pg-promise';
 
 const pgp = pgPromise({}); // Initialization options
-const db = pgp('postgres://postgres:password@nodedebug-postgres:5432/mydb');   
+const db = pgp('postgres://postgres:password@nodedebug-postgres:5432/mydb');
 
-app.get('/', (req, res) => {
-  let user: User = {
-    name: 'John Doe',
-    id: 1
-  };
 
-  db.one('SELECT $1 AS value', 123)
+app.get('/products', (req, res) => {
+  let products: Product[] = [];
+  db.many('SELECT * FROM products')
     .then((data) => {
-      console.log('DATA:', data.value)
+      for (const product of data) {
+        console.log(`Product: ${product.name}, Price: ${product.price}`);
+        let p: Product = {
+          name: product.name,
+          price: product.price
+        }
+        products.push(p);
+      }
+      res.json(products);
     })
     .catch((error) => {
       console.log('ERROR:', error)
+      res.status(500).send('Database error');
     })
-
-
-  res.send('Hello World!')
 })
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
